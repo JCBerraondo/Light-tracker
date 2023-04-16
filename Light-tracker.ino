a6c947ac572a6d2c35141344cc78052568132bce
Juan Cruz Berraondo <130011035+JCBerraondo@users.noreply.github.com>
2023-04-16 13:19:20 -0300

    Update Light-tracker.ino

--- Light-tracker.ino
+++ Light-tracker.ino
@@ -1,38 +1,38 @@
-// Definición de variables salida
+// Outputs
   const int ledTest= 13;
 
-// variables de entrada
+// Inputs
   const int fI= A0;
   const int fD= A1;
 
-// Motores
+// Engines
   int AIA= 3;
   int AIB= 11;
   int BIA= 10;
   int BIB= 6;
 
-// Resistividad
+// Photoresists
   int rI=0;
   int rD=0;
 
 void setup() {
   Serial.begin(9600);
 
-  // Iniciar pines de salida
+  // Setting outputs
     pinMode(ledTest, OUTPUT);  
     pinMode(AIA, OUTPUT);
     pinMode(AIB, OUTPUT);
     pinMode(BIA, OUTPUT);
     pinMode(BIB, OUTPUT);
 
-  //Pines apagados
+  //Turns off outputs
     digitalWrite(ledTest, LOW);
     analogWrite(AIA, 0);
     analogWrite(BIA, 0);
     analogWrite(AIB, 0);
     analogWrite(BIB, 0);
 
-  // Saludo inicial
+  // Beginning greeting
     for (int i=0; i<10; i++){
       digitalWrite(ledTest, HIGH);
       delay(50);
@@ -43,7 +43,7 @@ void setup() {
 }
 
 void loop() {
-  // Lectura de fotoresistencias
+  // Read photoresists value
   rI= analogRead(fI);
   rD= analogRead(fD);
 
@@ -52,47 +52,47 @@ void loop() {
   Serial.println(rD);
 
 
-  // Condiciones
-    if (rD<500 && rI<500){       //obscuridad=1023; claridad=0
-      adelante();
+  // Conditions
+    if (rD<500 && rI<500){       //darkness=1023; clarity=0
+      forward();
     }
     else if (rD<500 && rI>500){
-      izquierda();
+      left();
       delay(10);
     }
     else if (rD>500 && rI<500){
-      derecha();
+      right();
       delay(10);
     }
     else{
-      busqueda();
+      search();
       delay(10);
     }
 }
 
-//Movimientos
-void adelante(){
+//Motion
+void forward(){
   analogWrite(AIA,0);
   analogWrite(AIB,180);
   analogWrite(BIA,180);
   analogWrite(BIB,0);
 }
 
-void derecha(){
+void right(){
   analogWrite(AIA,0);
   analogWrite(AIB,180);
   analogWrite(BIA,0);
   analogWrite(BIB,0);
 }
 
-void izquierda(){
+void left(){
   analogWrite(AIA,0);
   analogWrite(AIB,0);
   analogWrite(BIA,180);
   analogWrite(BIB,0);
 }
 
-void busqueda(){
+void search(){
   analogWrite(AIA,0);
   analogWrite(AIB,120);
   analogWrite(BIA,0);
